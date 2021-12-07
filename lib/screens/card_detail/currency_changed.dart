ba7fa98144132cb7b579f8bb3c57413cc8062213
Dung Nguyen <nguyenqdung16@gmail.com>
2021-12-07 22:29:04 +0700

    added curency changed

---
+++ lib/screens/card_detail/currency_changed.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class CurrencyChanged extends StatelessWidget {
+  const CurrencyChanged({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            children: const [
+              Text(
+                'USD',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(width: 10),
+              Text(
+                '72,26',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Icon(
+                Icons.arrow_downward_rounded,
+                size: 20,
+                color: Colors.red,
+              ),
+            ],
+          ),
+          Row(
+            children: const [
+              Text(
+                'EUR',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(width: 10),
+              Text(
+                '40,40',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(width: 10),
+              Icon(
+                Icons.arrow_downward_rounded,
+                size: 20,
+                color: Colors.red,
+              ),
+            ],
+          ),
+          Row(
+            children: const [
+              Text(
+                'GBP',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(width: 10),
+              Text(
+                '265,01',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(width: 10),
+              Icon(
+                Icons.arrow_upward,
+                size: 20,
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
