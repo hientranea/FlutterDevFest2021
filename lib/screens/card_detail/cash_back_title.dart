28fceeef8c58f9a79350a9a89ec53ba3e40480d6
Dung Nguyen <nguyenqdung16@gmail.com>
2021-12-07 22:29:45 +0700

    added cash back title

---
+++ lib/screens/card_detail/cash_back_title.dart
@@ -0,0 +1,44 @@
+import 'package:crypto_tracker/config/crypto_tracker_color.dart';
+import 'package:flutter/material.dart';
+
+class CashBackTitle extends StatelessWidget {
+  const CashBackTitle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            'Cash Back',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 22.0,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+          Row(
+            children: const [
+              Text(
+                'Sea all',
+                style: TextStyle(
+                  color: CryptoTrackerColors.primaryColor,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Icon(
+                Icons.navigate_next_outlined,
+                color: CryptoTrackerColors.primaryColor,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
