165edc0267d5d3684592cbdbe8ba5680d14f08b2
Hien Tran <tranphuochien96@gmail.com>
2021-12-07 23:17:45 +0700

    Update CoinMarket production credential

--- lib/repos/crypto_tracker_services.dart
+++ lib/repos/crypto_tracker_services.dart
@@ -28,7 +28,7 @@ class CryptoTrackerServicesImpl implements CryptoTrackerServices {
 
   // ALERT: Just for demo purpose
   // Does not do it in production product :(
-  static const String _apiKey = "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c";
+  static const String _apiKey = "d06af65b-ac19-4941-8a6b-98527dcc18de";
 
   factory CryptoTrackerServicesImpl() {
     return _singleton;
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static String urlBase = "https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/";
+  static String urlBase = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/";
   static const int connectionTimeout = 10000;
   static String apiListingCrypto = "listings/latest?start=1&limit=10&convert=USD";
   static const icSplashLogo = "assets/images/ic_splash_logo.gif";
